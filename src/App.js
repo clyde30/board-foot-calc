7b80a9603e714e70f024a4f3eb2478f6e29c861e
Austin Jones <ajonestx@gmail.com>
2020-08-08 22:51:59 -0600

    restyle

--- src/App.js
+++ src/App.js
@@ -6,65 +6,95 @@ function calculateBoardFeet(width, length, thickness) {
 }
 
 export default function App() {
-  const [width, setWidth] = useState(0);
-  const [length, setLength] = useState(0);
-  const [thickness, setThickness] = useState(0);
-  const [price, setPrice] = useState(0);
+  const [width, setWidth] = useState();
+  const [length, setLength] = useState();
+  const [thickness, setThickness] = useState();
+  const [price, setPrice] = useState();
   const bf = calculateBoardFeet(width, length, thickness);
   const amount = bf * price
 
   return (
-    <div className="App">
-      <div className="container">
+    <div className="container">
+      <div className="inputs-container">
         <div className="input">
-          <label for="price" className="input-label">Price:</label>
-          <input
-            className="input-value"
-            type="number"
-            placeholder={price}
-            value={price}
-            onChange={e => setPrice(e.target.value)}
-          />
-        </div>
-        <div className="input">
-            <label for="width" className="input-label">Width:</label>
-            <input
-              className="input-value"
-              type="number"
-              placeholder={width}
-              value={width}
-              onChange={e => setWidth(e.target.value)}
-            />
-        </div>
-        <div className="input">
-          <label for="length" className="input-label">Length:</label>
-          <input
-          className="input-value"
-            type="number"
-            placeholder={length}
-            value={length}
-            onChange={e => setLength(e.target.value)}
-          />
-        </div>
-        <div className="input">
-          <label for="thickness" className="input-label">Thickness:</label>
-          <select className="input-value" id="thickness" value={thickness} onChange={e => setThickness(e.target.value)}>
-            <option value=".50">1/2</option>
-            <option value=".75">3/4</option>
-            <option value="1">4/4</option>
-            <option value="1.25">5/4</option>
-            <option value="1.50">6/4</option>
-            <option value="2">8/4</option>
-          </select>
-        </div>
-        <div className="solution">
           <div>
-            <span>{bf} BF</span>
+            <label>Price</label>
           </div>
           <div>
-            <span>${amount}</span>
+            <input type="text" placeholder="$" value={price} onChange={e => setPrice(e.target.value)}/>
           </div>
         </div>
+        <div className="input">
+          <div>
+            <label>Length(ft)</label>
+          </div>
+          <div>
+            <select value={length} onChange={e => setLength(e.target.value)}>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+              <option>6</option>
+              <option>7</option>
+              <option>8</option>
+              <option>9</option>
+              <option>10</option>
+              <option>11</option>
+              <option>12</option>
+              <option>13</option>
+              <option>14</option>
+              <option>15</option>
+              <option>16</option>
+            </select>
+          </div>
+        </div>
+        <div className="input">
+          <div>
+            <label>Width(in)</label>
+          </div>
+          <div>
+            <select value={width} onChange={e => setWidth(e.target.value)}>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+              <option>6</option>
+              <option>7</option>
+              <option>8</option>
+              <option>9</option>
+              <option>10</option>
+              <option>11</option>
+              <option>12</option>
+              <option>13</option>
+              <option>14</option>
+              <option>15</option>
+              <option>16</option>
+              <option>17</option>
+              <option>18</option>
+              <option>19</option>
+              <option>20</option>
+            </select>
+          </div>
+        </div>
+        <div className="input">
+          <div>
+            <label>Thickness</label>
+          </div>
+          <div>
+            <select value={thickness} onChange={e => setThickness(e.target.value)}>
+              <option value="1">4/4</option>
+              <option value="1.25">5/4</option>
+              <option value="1.5">6/4</option>
+              <option value="2">8/4</option>
+              <option value="3">12/4</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <div className="totals-container">
+        ${amount}
       </div>
     </div>
   );
