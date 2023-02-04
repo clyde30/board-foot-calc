0ef3f41efcea794d5549531922c50369dc05ecd0
Austin Jones <ajonestx@gmail.com>
2023-02-03 18:56:51 -0700

    add comment

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,22 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Calculator from '../components/calculator'
-import Header from '../components/Header'
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const Home: NextPage = () => {
+
+  const { data: session } = useSession();
+
+
+  // todo redirect to login if not logged in
   return (
     <div className='flex items-center justify-center bg-white h-screen'>
       <Head>
         <title>Board Foot Calculator</title>
       </Head>
 
+
       <main>
-        <Header />
         <Calculator />
       </main>
     </div>
